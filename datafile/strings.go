befe3097644b5054f88069b106866712fcb3c6cc
ap <evilgamer163@yandex.ru>
2023-12-28 09:27:23 +0400

    add datafile pkg

---
+++ datafile/strings.go
@@ -0,0 +1,28 @@
+package datafile
+
+import (
+	"bufio"
+	"os"
+)
+
+func GetStrings(filePath string) ([]string, error) {
+	var lines []string
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	err = file.Close()
+	if err != nil {
+		return nil, err
+	}
+	if scanner.Err() != nil {
+		return nil, scanner.Err()
+	}
+
+	return lines, nil
+}
