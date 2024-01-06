f018a624e968a3141ebe6e5667bb7068668c1ee2
ap <evilgamer163@yandex.ru>
2024-01-06 10:34:01 +0400

    add color

--- decoration/decoration.go
+++ decoration/decoration.go
@@ -5,13 +5,22 @@ import (
 	"strings"
 )
 
+// colorRed := "\033[31m"
+// colorYellow := "\033[33m"
+
 func SetTheme(str string) {
-	fmt.Println("<<-- ", str, " -->>")
-	fmt.Println("<<-------------------------------->>")
+	colorReset := "\033[0m"
+	colorGreen := "\033[32m"
+
+	fmt.Println(string(colorGreen), "<<-- ", str, " -->>")
+	fmt.Println("<<-------------------------------->>", string(colorReset))
 }
 
 func SetSubTheme(str string) {
+	colorReset := "\033[0m"
+	colorCyan := "\033[36m"
+
 	fmt.Println()
-	fmt.Println("*", strings.ToUpper(str))
-	fmt.Println("********")
+	fmt.Println(string(colorCyan), "*", strings.ToUpper(str))
+	fmt.Println("********", string(colorReset))
 }
